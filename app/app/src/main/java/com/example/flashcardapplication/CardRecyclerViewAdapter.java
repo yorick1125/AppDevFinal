35024fecffa971816baa9cdff94eb74e41dc6764
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2021-11-24 12:18:03 -0500

    fixed edit card fragment bug

--- app/app/src/main/java/com/example/flashcardapplication/CardRecyclerViewAdapter.java
+++ app/app/src/main/java/com/example/flashcardapplication/CardRecyclerViewAdapter.java
@@ -7,46 +7,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.flashcardapplication.placeholder.PlaceholderContent.PlaceholderItem;
+import com.example.flashcardapplication.model.Card;
 import com.example.flashcardapplication.databinding.FragmentCardListItemBinding;
 
 import java.util.List;
 
 /**
- * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * {@link RecyclerView.Adapter} that can display a {@link Card}.
  * TODO: Replace the implementation with code for your data type.
  */
 public class CardRecyclerViewAdapter extends RecyclerView.Adapter<CardRecyclerViewAdapter.ViewHolder> {
 
-    private final List<PlaceholderItem> mValues;
+    private final List<Card> cards;
 
-    public CardRecyclerViewAdapter(List<PlaceholderItem> items) {
-        mValues = items;
+    public CardRecyclerViewAdapter(List<Card> items) {
+        cards = items;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
         return new ViewHolder(FragmentCardListItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
 
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        holder.card = cards.get(position);
+        holder.mIdView.setText(cards.get(position).getId().intValue());
+        holder.mContentView.setText(cards.get(position).getFront());
     }
 
     @Override
     public int getItemCount() {
-        return mValues.size();
+        return cards.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final TextView mIdView;
         public final TextView mContentView;
-        public PlaceholderItem mItem;
+        public Card card;
 
         public ViewHolder(FragmentCardListItemBinding binding) {
             super(binding.getRoot());
