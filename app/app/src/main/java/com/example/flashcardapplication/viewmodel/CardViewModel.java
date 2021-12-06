c5aeae81282dcb8a76633fc0d10e647baa841706
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2021-12-06 12:32:11 -0500

    created card view model

---
+++ app/app/src/main/java/com/example/flashcardapplication/viewmodel/CardViewModel.java
@@ -0,0 +1,47 @@
+package com.example.flashcardapplication.viewmodel;
+
+import com.example.flashcardapplication.model.Card;
+import com.example.flashcardapplication.model.Deck;
+
+public class CardViewModel extends ObservableModel<CardViewModel> {
+
+    public enum State { NONE, BEFORE_EDIT, EDITED, BEFORE_CREATE, CREATED }
+
+    private State state;
+    private Card card;
+    private Card updatedCard;
+
+    public CardViewModel() {
+        state = State.NONE;
+    }
+
+    @Override
+    protected CardViewModel get() {
+        return this;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Card getUpdatedDeck() {
+        return updatedCard;
+    }
+
+    public void setUpdatedDeck(Card updatedCard) {
+        this.updatedCard = updatedCard;
+    }
+}
